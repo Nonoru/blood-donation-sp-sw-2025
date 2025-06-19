2608d4bcba1b65234e444178628f37028bcd1c40
Nonoru <hoannaa2011@gmail.com>
2025-06-19 17:39:55 +0700

    merge be to main branch

--- src/main/java/com/nonoru/superapp/repository/OrderBloodDonationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nonoru.superapp.repository;
-
-import com.nonoru.superapp.entity.OrderBloodDonation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderBloodDonationRepository extends JpaRepository<OrderBloodDonation, Long> {
-}
--- src/main/java/com/nonoru/superapp/service/OrderDateDonationService.java
+++
@@ -1,7 +0,0 @@
-package com.nonoru.superapp.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class OrderDateDonationService {
-}
